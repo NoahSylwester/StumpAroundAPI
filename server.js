93d4c5d9e1ad7e74924ea828227ec616a2af0104
Noah Sylwester <nanoabram@comcast.net>
2019-12-03 21:20:09 -0800

    initialize files and basic schema

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const moment = require('moment');
+
+// Require all models
+var db = require("./models");
+
+const PORT = process.env.PORT || 8080;
+
+// Initialize Express
+const app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/StumpAround";
+// // Connect to the Mongo DB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// example routes
+app.get("/", function(req, res) {
+    res.json('GET');
+});
+  
+app.post("/", function(req, res) {
+    res.json('POST');
+});
+
+// Start the server
+app.listen(PORT, function() {
+console.log("App running on port " + PORT + "!");
+});
