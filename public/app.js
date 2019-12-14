7662dd3f60991bdcd6be4a96db8b845e52172ba7
Fearghus Tyler <Fearghus333@gmail.com>
2019-12-14 13:28:13 -0800

    make better

--- public/app.js
+++ public/app.js
@@ -1,9 +1,20 @@
-$("#hikes").click(function () {
+$("#login").click(function () {
     console.log("deadoralive")
     $.post("http://localhost:8080/login",
         {
             email: "test",
             password: "test",
+        }
+    )
+})
+
+$("#signup").click(function () {
+    console.log("deadoralive")
+    $.post("http://localhost:8080/sigunup",
+        {   username:"test",
+            email: "test",
+            password: "test",
+            confirm:"test"
         }
     )
 })
