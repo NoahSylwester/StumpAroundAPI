6e2041ab4e11a70cd3bfe4ddd2e60f7f774c1390
Noah Sylwester <nanoabram@comcast.net>
2020-01-04 16:57:51 -0800

    edit select statement

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.get("/user/:username", function (req, res) {
     db.User.findOne({
         name: name
     })
-        .select('password')
+        .select('-password')
         .populate("comments")
         .populate({
             path: "profileComments",
